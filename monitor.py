e2b5e6b9c0c74d7aea1ebbfeca13e881524d0a40
Andrew Tu <drewtu2@yahoo.com>
2017-04-21 00:22:52 -0400

    Updated gitignore and monitor.py
    Gitignore no longer excludes "MidasMonitor" (changed the venv directory
    on local)
    monitor.py now fully capable of reporting GPU temps

--- monitor.py
+++ monitor.py
@@ -1,24 +1,48 @@
 import psutil
 from samplePsutil import testData
-
+from datetime import datetime
 
 class monitor:
 
   def __init__(self):
-    sensorTempResponse = psutil.sensors_temperatures()
-    self.amdGpus = sensorTempResponse("amdgpu")
-    
-  def printGpuTemp(self):
-    count = 0;
-    for gpu in self.amdGpus.items():
-      print("AMD GPU " + count)
-      print("*" * 10)
-      print("label: " + gpu(0))
-      print("Current Temp: " + gpu(1))
-      print("High Temp: " + gpu(2))
-      print("CriticalTemp: " + gpu(3))
-      print()
+    #sensorTempResponse = psutil.sensors_temperatures()
+    #self.amdGpus = sensorTempResponse("amdgpu")
+    self.amdGpus = testData["amdgpu"]
+    self.logfileName = "midas.log"
+
+  def recordTemps(self):
+    with open(self.logfileName, "a") as logfile:
+      logfile.write(str(datetime.now()) + "\n")
+      logfile.write("*" * 80)
+      logfile.write("\n")
+      logfile.write(self.getGpuTempList())
+      logfile.write("*" * 80 + "\n")
+
+  # Takes a list of GPU shwtemps and returns them in a printable format
+  # List of shwtemps => String
+  def getGpuTempList(self):
+    count = 0
+    printout = ""
+    for gpu in self.amdGpus:
+      printout += self.getGpuShwtemps(gpu, count)
       count += 1
+    return printout
+ 
+  # Takes a GPU shwtemp and returns it in a printable format 
+  # GpuShwtemp + Number => String
+  def getGpuShwtemps(self, gpu, count):
+    printout = \
+      "AMD GPU " + str(count) + "\n"\
+       + "*" * 10 + "\n"\
+       + "label: " + gpu[0] + "\n"\
+       + "Current Temp: " + str(gpu[1]) + "\n"\
+       + "High Temp: " + str(gpu[2]) + "\n"\
+       + "CriticalTemp: " + str(gpu[3]) + "\n\n"
+    return printout
+
+  # Prints all current GPU temps to the screen
+  def printGpuTempList(self):
+    print(self.getGpuTempList())
 
 m = monitor()
-m.printGpuTemp()
+m.recordTemps()
