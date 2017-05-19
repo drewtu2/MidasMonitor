9ade9aceb9cbb2831014760092f76a9c9c264fce
Andrew Tu <drewtu2@yahoo.com>
2017-05-19 00:40:08 -0400

    Added basic routing. Testing GroupMe hook

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, requests
 
 app = Flask(__name__)
 
+# Route to the basic "Hello World" page.... 
 @app.route("/")
 def hello():
   return "Hello world!"
+
+# Handle the GroupMe Notification of a new message that has arrived
+@app.route("/bot", methods=["POST"])
+def handleGroupme():
+  print(request.get_json)
+
+# Handle the arrival of a miner status update. 
+@app.route("/localDump", methods["POST"])
+def handleMinerUpdate:
+  print
+  
 
 
 if __name__ == "__main__":
