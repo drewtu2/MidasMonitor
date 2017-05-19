62f7199eb03916ccf19d7abe5e425ee954207953
Andrew Tu <drewtu2@yahoo.com>
2017-05-19 01:31:52 -0400

    added groupme

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
+import groupy
 from flask import Flask, request
 
+# Initialization
 app = Flask(__name__)
+g = groupy.Group.list().filter(group_id=os.environ.get("GROUPME_GROUPID")).first
+
 
 # Route to the basic "Hello World" page.... 
 @app.route("/")
@@ -10,13 +14,13 @@ def hello():
 
 # Handle the GroupMe Notification of a new message that has arrived
 @app.route("/bot", methods=["POST"])
-def handleGroupme():
-  print(request.get_json)
+def botCallback():
+  handleBotCallback()
 
 # Handle the arrival of a miner status update. 
 @app.route("/localDump", methods=["POST"])
 def handleMinerUpdate():
-  print
+  print("")
   
 
 
@@ -24,3 +28,5 @@ if __name__ == "__main__":
   port = int(os.environ.get("PORT", 5000))
   app.run(host='0.0.0.0', port=port)
 
+def handleBotCallback():
+  print(g.messages().newest.text)
