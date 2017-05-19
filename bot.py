feb5b77e0d65eb2e453fb9b77a5c3ee38bab0410
Andrew Tu <drewtu2@yahoo.com>
2017-05-19 00:04:49 -0400

    Added profile and app.py

---
+++ bot.py
@@ -0,0 +1,32 @@
+import os
+import random
+from flask import Flask, json, request
+import requests
+from monitor import monitor
+app = Flask(__name__)
+m = monitor()
+
+def reply(message):
+  payload = {\
+  'bot_id' : os.environ['BOT_ID'],\
+  'text'   : message}
+  requests.post('https://api.groupme.com/v3/bots/post', json=payload)
+
+@app.route('/', methods=['POST'])
+def groupme_callback():
+  json_body = request.get_json()
+  if json_body['group_id'] == os.environ['GROUP_ID'] and json_body['sender_type'] != 'bot':
+  # some degree of verification that it is sent via a groupme callback
+  # could also check for "User-Agent: GroupMeBotNotifier/1.0", but that's plenty spoofable
+    message = json_body['text']
+    message = m.printGpus()
+    reply(message)
+
+@app.route('/hello', methods=['GET'])
+def helloWorld():
+  return "hello world!"
+
+if __name__ == "__main__":
+  port = int(os.environ.get("PORT", 5000))
+  # app.run(host='0.0.0.0', port=port, debug=True)
+  app.run(host='0.0.0.0', port=port)
