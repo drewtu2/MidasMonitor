278f92f5e716c104369ccc989afca84055732790
Andrew <drewtu2@gmail.com>
2017-05-24 22:39:48 -0400

    updated worker script

--- scripts/runWorker.sh
+++ scripts/runWorker.sh
@@ -1,6 +1,8 @@
-#!/bash/bin
 
 export MIDAS_HOME=~/Documents/MidasMonitor/
+echo MIDAS_HOME = $MIDAS_HOME
 source $MIDAS_HOME/setup
+
 source $MIDAS_HOME/venv/bin/activate
+
 python $MIDAS_HOME/lib/worker.py
