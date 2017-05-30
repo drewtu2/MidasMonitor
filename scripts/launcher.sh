64054000bab81c1ada61a270a4015301da6aedea
Andrew Tu <drewtu2@yahoo.com>
2017-05-30 19:30:53 -0400

    Updated launchers, updated worker script to include log statement.

--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -27,21 +27,47 @@ source minerConfig
 # - Dwarfpool
 ###############################################################################
 
+##################
+# Constants
+##################
 
+# Wallet to mine to 
+AT_JB_WALLET="3c76329390da17c727fa1bbbeb2fc45c80a7d92f"
+WALLET=$AT_JB_WALLET
+
+# Name of the worker
+WORKER_NAME="Midas"
+EMAIL="drewtu2@yahoo.com"
+
+####################################
+# Run on ethermine with ethminer
+####################################
+_ethminer_ethermine() {
+  echo "Running Ethermine w/ Stratum" 
+  ethminer --farm-recheck 200 -G -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O $WALLET.$WORKER_NAME
+};
+
+####################################
+# Run on dwarfppool with ethminer
+####################################
+_ethminer_dwarfpool() {
+  echo "Running Dwarfpool"
+  ethminer -G -F http://eth-us.dwarfpool.com:80/$AT_JB_WALLET/$EMAIL
+};
+
+
+####################################
+# Based on arguments, determine what
+# to run...
+####################################
 if [[ $# -eq 0 ]];
 then
-  echo "Running Ethermine w/ Stratum" 
-  ethminer --farm-recheck 200 -G -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O 3c76329390da17c727fa1bbbeb2fc45c80a7d92f.Midas
-
+  _ethminer_ethermine
 elif [ "$1" == "ethermine" ];
 then
-  echo "Running Ethermine w/ Stratum" 
-  ethminer --farm-recheck 200 -G -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O 3c76329390da17c727fa1bbbeb2fc45c80a7d92f.Midas
-
+  _ethminer_ethermine
 elif [ "$1" == "dwarfpool" ];
 then
-  echo "Running Dwarfpool"    
-  ethminer -G -F http://eth-us.dwarfpool.com:80/3c76329390da17c727fa1bbbeb2fc45c80a7d92f/drewtu2@yahoo.com
-
+  _ethminer_dwarfpool
 fi
 
--- scripts/runWorker.sh
+++ scripts/runWorker.sh
@@ -1,8 +1,25 @@
+###############################################################################
+# Andrew tu
+# This script is intended to be run as a chron job every "N" minutes. 
+# This script is responsible for setting up and running worker.py to 
+# send a heartbeat to the Heroku server responsible for monitoring the 
+# ethereum miner...
+###############################################################################
 
-export MIDAS_HOME=~/Documents/MidasMonitor/
+
+# Set up Variables
+
+# Location on Midas
+export MIDAS_HOME="~/Documents/MidasMonitor/"
+
+# Location on Andrew MBPR
+export MIDAS_HOME="/Users/Andrew/Dropbox/Northeastern/Projects/MidasMonitor/"
+
 echo MIDAS_HOME = $MIDAS_HOME
-source $MIDAS_HOME/setup
 
+source $MIDAS_HOME/setup
 source $MIDAS_HOME/venv/bin/activate
 
-python $MIDAS_HOME/lib/worker.py
+#python $MIDAS_HOME/lib/worker.py
+
+echo Last run: $(date) >> $MIDAS_HOME/worker.log
