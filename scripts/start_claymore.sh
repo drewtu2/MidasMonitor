2d6f85b8892fa34ad160cf1b63cca61b4876e330
Andrew Tu <drewtu2@yahoo.com>
2018-12-13 02:52:18 -0500

    added easy start scripts

---
+++ scripts/start_claymore.sh
@@ -0,0 +1,3 @@
+cd ~/Desktop/MidasMonitor/scripts
+source minerConfig
+sudo ./Claymore/ethdcrminer64 configs/midas_eth_only.txt
---
+++ scripts/start_xmr.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+
+cd ~/Desktop/xmr-stak/build/bin
+./xmr-stak
